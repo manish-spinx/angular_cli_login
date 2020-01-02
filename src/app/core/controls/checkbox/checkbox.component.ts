7f71d4c9e6d3f07ae2c5ebb748db2d2da8d30f79
Manish Sharma <manishs@development.local>
2020-01-02 15:34:12 +0530

    checkbox issue solved

--- src/app/core/controls/checkbox/checkbox.component.ts
+++ src/app/core/controls/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,OnInit,Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
     selector: 'spinx-checkbox',
@@ -6,7 +6,24 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class checkboxComponent implements OnInit {
+
+    @Input() public label: string;
+    @Input() public class: string;
+    @Input() public id: string;
+    @Input() public name: string;
+    @Input() public record_list:any;
+    @Input() public submit_validaiton_flag:boolean = false; 
+    @Input() public selected_record_list:string[]=[]; 
+    
+
+    @Output() onClick = new EventEmitter<any>();
+
+
     constructor() { }
 
     ngOnInit() { }
+
+    onClickButton(event) {
+        this.onClick.emit(event);
+      }
 }
